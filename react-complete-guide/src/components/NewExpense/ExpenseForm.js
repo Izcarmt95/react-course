f41b20f5933cb8aa548125f59cb1355dc1cb5aee
Izcar J. Muñoz Torrez <izcarmt95@gmail.com>
2023-04-23 14:54:29 -0600

    feat(app): summit handler

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
@@ -17,8 +17,23 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     };
 
+    
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            price: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
